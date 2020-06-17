51d3df734c7b8a3419464fb664c6873e4a051fc7
arafat0529 <arafat0529@yahoo.com>
2020-06-16 18:04:46 -0700

    test case 7

--- src/test/java/com/cbt/tests/homework3/Testcase6_8.java
+++ src/test/java/com/cbt/tests/homework3/Testcase6_8.java
@@ -1,11 +1,17 @@
 package com.cbt.tests.homework3;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import javax.swing.*;
+
+import java.security.Key;
 
 import static com.cbt.utilities.BrowserFactory.getDriver;
 
@@ -67,5 +73,18 @@ public class Testcase6_8 {
         Assert.assertEquals(subject, "File Uploaded!");
         String fileName = driver.findElement(By.id("uploaded-files")).getText();
         Assert.assertEquals(fileName, "test.txt");
+    }
+
+    @Test
+    public void testCase8() throws Exception {
+        driver.get("https://practice-cybertekschool.herokuapp.com/");
+        Thread.sleep(2000);
+        driver.findElement(By.linkText("Autocomplete")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("myCountry")).sendKeys("United States of America");
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div/form/input")).click();
+        Thread.sleep(2000);
+        String message = driver.findElement(By.id("result")).getText();
+        Assert.assertEquals(message, "You selected: United States of America");
     }
 }
